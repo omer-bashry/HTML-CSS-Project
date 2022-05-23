25786a6e7b72dd1c5d04ef6a64c39b1b964c36e8
JoeMicro240528 <yousefalboshra@gmail.com>
2022-05-23 22:30:14 +0300

    add header section to all pages

---
+++ js/header.js
@@ -0,0 +1,66 @@
+let Headerpage = document.querySelector("header");
+
+Headerpage.innerHTML =
+ `
+  <nav class=" navbar navbar-expand-lg text-body ">
+		  <a class="navbar-brand  bg-Dark" href="index.html"><img class="logo" src="images/logo.png"></a>
+		      <img class="closemnue" src="images/close.png">
+		      <div class="openmnue">
+		     	<span></span>
+		  	    <span class="min-width"></span>
+		        <span></span>
+		      </div>
+	  <div class="collapse navbar-collapse">
+			  <ul class="navbar-nav ">
+			      <li class="active nav-item nav-link home ">
+			         Home 
+			      </li>
+			      <li class="nav-item nav-link">
+			           Men's
+			      </li>
+			       <li class="nav-item nav-link">
+			           Women's
+			      </li>
+			       <li class="nav-item nav-link">
+			           Kid's
+			      </li>
+			      <li class="nav-item nav-link dropdown openPagedrop">
+				     <div class=" dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle=" dropdown" aria-expanded="false">
+                            Pages
+				      </div>
+				        
+			      </li>
+
+			      <li class="nav-item nav-link dropdown openfeaturesdrop">
+				        <div class=" dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+				        Features
+				        </div>
+			      </li>
+			      <li class="nav-item nav-link">
+			         Explore
+			      </li>
+			  </ul>
+		 </div>
+		</nav>
+		  <div class="drop-page">
+			   	<ul>
+			 		<li class="about">About Us</li>
+					<li class="product">Products</li>
+	          		<li class="detailse">Single Product</li>
+					<li class="contact">Contact Us</li>
+			    </ul>
+          </div>
+          <div class="drop-Features">
+			   	<ul>
+			 		<li>Features Page 1</li>
+					<li>Features Page 2</li>
+	          		<li>Features Page 3</li>
+					<li>Template Page 4</li>
+			    </ul>
+          </div>
+          <div class="navsmall">
+          	    <ul class="small">
+			     
+			    </ul>
+          </div>
+`;
--- js/index.js
+++ js/index.js
@@ -1,8 +1,17 @@
 
 let pageDrop = document.querySelector(".drop-page ul");
 let featuresDrop = document.querySelector(".drop-Features ul");	
-document.querySelector(".openPagedrop").addEventListener("click",toggelPageDrop);
-document.querySelector(".openfeaturesdrop").addEventListener("click",toggelFeaturesDrop);
+let openMnue = document.querySelector(".openmnue");
+let closeMnue = document.querySelector(".closemnue");
+let navDrop = document.querySelector(".navsmall ul");
+let menNav = document.querySelector(".navbar-collapse ul");
+let openMnuePage = document.querySelector(".openPagedrop");
+let openMnueFeatuers = document.querySelector(".openfeaturesdrop");
+ 
+
+openMnuePage.addEventListener("click",toggelPageDrop);
+openMnueFeatuers.addEventListener("click",toggelFeaturesDrop);
+
 function toggelPageDrop(){
 
  featuresDrop.style.display == "block"?featuresDrop.style.display = "none":featuresDrop.style.display = "none";
@@ -11,11 +20,125 @@ function toggelPageDrop(){
 
 }
 
-function toggelFeaturesDrop(){
+function toggelFeaturesDrop() {
 
  pageDrop.style.display == "block"?pageDrop.style.display = "none":pageDrop.style.display = "none";
 
  featuresDrop.style.display == "block"?featuresDrop.style.display = "none":featuresDrop.style.display = "block";
 
 
+}
+
+(function toggelMnue(){	
+
+openMnue.addEventListener("click",OpenMnue);
+closeMnue.addEventListener("click",CloseMnue);
+
+})();
+
+function OpenMnue(){
+ navDrop.style.display = "block";
+ openMnue.style.display = "none";
+
+ setTimeout(() => {
+     closeMnue.style.display = "block";
+	}, 500);
+}
+
+function CloseMnue(){
+  navDrop.style.display = "none";
+  closeMnue.style.display = "none";
+
+ setTimeout(() => {
+     openMnue.style.display = "block";
+	}, 500);
+}
+
+let navContent = menNav.innerHTML;
+ navDrop.innerHTML = navContent;
+
+ document.querySelector(".small li.openPagedrop").innerHTML += `<div class="minMnue" style="display:none;"> ${pageDrop.innerHTML}</div>`;
+
+let smallPagedrop = document.querySelector(".small li.openPagedrop .minMnue");
+document.querySelector(".small li.openPagedrop").addEventListener("click", dropdwonPage);
+
+
+function dropdwonPage(){
+	if (smallFeatdrop.style.display == "block") {
+		smallFeatdrop.style.display = "none";
+	}
+
+	if (smallPagedrop.style.display == "block") {
+
+         document.querySelector(".navsmall .small").style.margin="207px 0 0 0";
+	     smallPagedrop.style.display = "none";
+	}else{
+
+        document.querySelector(".navsmall .small").style.margin="329px 0 0 0";
+	     smallPagedrop.style.display = "block";
+	}
+	
+}
+
+
+document.querySelector(".small li.openfeaturesdrop").innerHTML +=
+ `<div class="minfeat" style="display:none;"> ${featuresDrop.innerHTML}</div>`;
+
+let smallFeatdrop = document.querySelector(".small li.openfeaturesdrop .minfeat");
+document.querySelector(".small li.openfeaturesdrop").addEventListener("click", dropdwonfeat);
+
+function dropdwonfeat(){
+
+if (smallPagedrop.style.display == "block") {
+		smallPagedrop.style.display = "none"
+	}
+
+
+	if (smallFeatdrop.style.display == "block") {
+
+         document.querySelector(".navsmall .small").style.margin="207px 0 0 0";
+	     smallFeatdrop.style.display = "none";
+	}else{
+
+        document.querySelector(".navsmall .small").style.margin="329px 0 0 0";
+	     smallFeatdrop.style.display = "block";
+	}
+	
+}
+
+document.querySelector(".home").addEventListener("click" , homepage);
+
+function homepage(){
+	setTimeout(() => {
+         window.location = "index.html";
+	}, 1000);
+}
+
+document.querySelector(".about").addEventListener("click" , aboutpage);
+
+function aboutpage(){
+	setTimeout(() => {
+         window.location = "about.html";
+	}, 1000);
+}
+document.querySelector(".contact").addEventListener("click" , contactpage);
+
+function contactpage(){
+	setTimeout(() => {
+         window.location = "contact.html";
+	}, 1000);
+}
+document.querySelector(".product").addEventListener("click" , productpage);
+
+function productpage(){
+	setTimeout(() => {
+         window.location = "products.html";
+	}, 1000);
+}
+document.querySelector(".detailse").addEventListener("click" , detailsepage);
+
+function detailsepage(){
+	setTimeout(() => {
+         window.location = "single-product.html";
+	}, 1000);
 }
